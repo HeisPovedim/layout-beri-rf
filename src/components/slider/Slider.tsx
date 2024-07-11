a20282f3e5129fe3ca445fad89e4ff4037368bb9
Mikhail Larionov <larionov404.mu@gmail.com>
2024-07-11 11:26:43 +0300

    commit

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,27 +1,60 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import RCSlider from "rc-slider";
+import "rc-slider/assets/index.css";
+import style from "./Slider.module.scss";
 
 const Slider: React.FC = () => {
-  const [value, setValue] = useState(1000);
+  const [value, setValue] = useState<number>(1000);
+  const sliderRef = useRef<HTMLDivElement>(null);
+  const valueRef = useRef<HTMLSpanElement>(null);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(parseInt(event.target.value, 10));
+  const handleChange: (newValue: number | number[]) => void = (newValue) => {
+    if (Array.isArray(newValue)) {
+      setValue(newValue[0]);
+    } else {
+      setValue(newValue);
+    }
   };
 
+  useEffect(() => {
+    const updateValuePosition = () => {
+      if (sliderRef.current && valueRef.current) {
+        const sliderWidth = sliderRef.current.offsetWidth;
+        const valueWidth = valueRef.current.offsetWidth;
+        const position =
+          ((value - 1000) / (100000 - 1000)) * (sliderWidth - valueWidth);
+
+        const maxPosition = sliderWidth - valueWidth;
+        const adjustedPosition = Math.min(position, maxPosition);
+
+        valueRef.current.style.left = `${adjustedPosition}px`;
+      }
+    };
+
+    updateValuePosition(); // Вызываем функцию при монтировании
+
+    window.addEventListener("resize", updateValuePosition);
+    return () => window.removeEventListener("resize", updateValuePosition);
+  }, [value]); // Добавляем value в зависимости
+
   return (
-    <div className="slider">
-      <input
-        type="range"
-        min="1000"
-        max="100000"
-        step="1000"
-        value={value}
-        onChange={handleChange}
-        className="w-full"
-      />
-      {/* <span className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-full bg-white px-2 py-1 rounded-md text-sm shadow">
-        {value}
-      </span> */}
+    <div className={style.sliderWrapper}>
+      <div ref={sliderRef} className={style.sliderTrack}>
+        {" "}
+        {/* Добавлен ref и класс для трека */}
+        <RCSlider
+          min={1000}
+          max={100000}
+          step={500}
+          onChange={handleChange}
+          value={value}
+        />
+        <span ref={valueRef} className={style.sliderValue}>
+          {value}
+        </span>{" "}
+        {/* Добавлен ref для значения */}
+      </div>
     </div>
   );
 };
